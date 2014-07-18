cd21c04147bce49d0432e574bfd84e9be53f5479
Ken Mamitsuka <ken@airtime.com>
2014-07-17 22:17:51 -0700

    added stuff

--- vline_yum_repos/recipes/default.rb
+++ vline_yum_repos/recipes/default.rb
@@ -1,18 +1,46 @@
 #
-# Cookbook Name:: centos_yum_repo
+# Cookbook Name:: vline_yum_repos
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
+# Copyright (C) 2014 Airtime
 #
 # All rights reserved - Do Not Redistribute
 #
 
+# install s3 yum tools
+git "/tmp" do
+  repository "https://github.com/seporaitis/yum-s3-iam"
+  revision "master"
+  action :sync
+end
+
+cookbook_file "/usr/lib/yum-plugins/s3iam.py" do
+  source "/tmp/yum-s3-iam/s3iam.py"
+  owner "root"
+  group "root"
+  mode 00755
+  action :create_if_missing
+end
+
+cookbook_file "/etc/yum/pluginconf.d/s3iam.conf" do
+  source "/tmp/yum-s3-iam/s3iam.conf"
+  owner "root"
+  group "root"
+  mode 00644
+  action :create_if_missing
+end
+
+# setup vline-libcxx repo
 yum_repository "vline-libcxx" do
   description "vline libcxx s3 yum repository"
   baseurl "http://airtime-stage-repos.s3.amazonaws.com/libcxx"
   gpgcheck false
-  source "s3_enabled=1"
   action :create
+end
+
+execute "add special s3_enabled flag for libcxx" do
+  user "root"
+  command "echo s3_enabled=1 >> /etc/yum.repos.d/vline-libcxx.repo"
 end
 
 yum_package "vline-libcxx" do
